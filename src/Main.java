1501bed09ead4e1dd179bcd493a612d031c8f08b
Марина Жукова <marinazhukova807@gmail.com>
2022-05-07 11:23:34 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketCost = 1210;
+        int bonus = 20;
+        int bonusMiles = ticketCost / bonus;
+        System.out.println("Начислено бонусных миль:" + bonusMiles);
+    }
+}
